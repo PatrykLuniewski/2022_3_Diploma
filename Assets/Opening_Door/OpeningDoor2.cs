b730aa96f166072570912511583da5d88b9f1ef2
PatrykLuniewski <s22768@pjwstk.edu.pl>
2023-03-01 02:32:23 +0100

    Opening door animation
    Opening door animation

--- Assets/Opening_Door/OpeningDoor2.cs
+++ Assets/Opening_Door/OpeningDoor2.cs
@@ -3,18 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpeningDoor : MonoBehaviour
+public class OpeningDoor2 : MonoBehaviour
 {
     // Start is called before the first frame update
     public Animator Door = null;
 
     public void OnTriggerEnter(Collider other)
     {
-        Console.WriteLine("Hello World!");
+
         if (other.CompareTag("Player"))
         {
-            Console.WriteLine("Hello World!");
-            Door.Play("DoorOpen", 0, 0.0f);
+
+            Door.Play("DoorOpening2", 0, 0.0f);
         }
     }
 }
