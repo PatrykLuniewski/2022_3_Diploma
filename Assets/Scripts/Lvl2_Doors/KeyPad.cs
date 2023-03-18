de00369d3729f99169cb1a9e287a069406a30c64
Mateusz <81259696+s20768@users.noreply.github.com>
2023-03-18 23:14:34 +0100

    lvl 2 door

---
+++ Assets/Scripts/Lvl2_Doors/KeyPad.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class KeyPad : MonoBehaviour
+{
+    [SerializeField] private TMP_Text Ans;
+    [SerializeField] private Animator Door;
+
+    private string Answer = "9999" ;
+
+    public void Number(int number)
+    {
+        Ans.text += number.ToString();
+    }
+   
+    public void Execute()
+    {
+        if(Ans.text == Answer)
+        {
+            Ans.text = "Correct";
+            Door.SetBool("Open", true);
+            StartCoroutine("StopDoor");
+        }
+        else
+        {
+            Ans.text = "B³¹d";
+        }
+    }
+    IEnumerator StopDoor()
+    {
+        yield return new WaitForSeconds(0.8f);
+        Door.SetBool("Open", false);
+        Door.enabled = false;
+    }
+}
