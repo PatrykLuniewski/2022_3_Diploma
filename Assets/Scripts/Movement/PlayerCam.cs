f0b24ad894e96d3030cae97b64cee92a6c2b7934
Mateusz <81259696+s20768@users.noreply.github.com>
2023-12-18 15:07:17 +0100

    lvl2

--- Assets/Scripts/Movement/PlayerCam.cs
+++ Assets/Scripts/Movement/PlayerCam.cs
@@ -7,18 +7,21 @@ public class PlayerCam : MonoBehaviour
 {
     public float sensX;
     public float sensY;
+    
 
     public Transform orientation;
 
     float xRotation;
     float yRotation;
 
+
     private void Start() {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    private void Update() {
+    private void Update()
+    {
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
@@ -28,5 +31,6 @@ public class PlayerCam : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
     }
 }
